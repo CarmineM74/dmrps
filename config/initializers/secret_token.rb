a7095ed60d91c0f4d8cb461240c30fe94f6eb820
Carmine Moleti <carmine.moleti@gmail.com>
2012-08-27 19:12:58 +0200

    Initial commit Rails new application just created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmrps::Application.config.secret_token = 'df70a699f123047519877fa6aac4e40d93c42d1df72181fa6015a8923bf5208610c74ad375f964f0a0ec559874cecbede92f40fdd5c89cb22c6afed9a2077399'

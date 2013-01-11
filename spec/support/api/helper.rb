fa27ba294a63d4a9dc1f81268d6f950adcbdc14b
Carmine Moleti <carmine@moleti.it>
2013-01-11 09:49:26 +0100

    Server Side Specs adjustments to deal with authentication
    Added two spec helpers:
    
    login_as(user)
    and
    logout()
    
    The first one stubs ApplicationController#current_user so that it
    returns the user passed via parameters.
    This allows us to impersonate a given user (with its roles and
    everything) during a spec.
    
    Likewise, the second helper, stubs the same method as before except this
    time we are always returning a nil. This has the net effect of a logout
    since no current_user is defined anymore.

--- spec/support/api/helper.rb
+++ spec/support/api/helper.rb
@@ -3,6 +3,17 @@ module ApiHelper
   def app
     Rails.application
   end
+
+  def login_as(user)
+    puts "Logging in as #{user.email} ..."
+    ApplicationController.any_instance.stub(:current_user).and_return(user)
+  end
+
+  def logout
+    puts "Logging out ..."
+    ApplicationController.any_instance.stub(:current_user).and_return(nil)
+  end
+
 end
 
 RSpec.configure do |config|

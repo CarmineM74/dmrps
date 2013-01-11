4f12b8fded5568f589f97571c4207e79a0bde2dd
Carmine Moleti <carmine@moleti.it>
2013-01-11 09:50:08 +0100

    Server Side Spec helpers
    Removed debug output from login_as and logout helpers.

--- spec/support/api/helper.rb
+++ spec/support/api/helper.rb
@@ -5,12 +5,10 @@ module ApiHelper
   end
 
   def login_as(user)
-    puts "Logging in as #{user.email} ..."
     ApplicationController.any_instance.stub(:current_user).and_return(user)
   end
 
   def logout
-    puts "Logging out ..."
     ApplicationController.any_instance.stub(:current_user).and_return(nil)
   end
 

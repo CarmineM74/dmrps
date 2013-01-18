aa539d20fb0aea57726dbaabb064f4d61ce1baed
Carmine Moleti <carmine@moleti.it>
2013-01-18 09:44:14 +0100

    Server Side SPECS Tweaked specs to take into account authentication requirements. Fixed users_spec that was failing due users' API now requiring authentication.

--- spec/apis/v1/users_spec.rb
+++ spec/apis/v1/users_spec.rb
@@ -4,24 +4,32 @@ describe "/api/v1/users.json", :type => :api do
   let(:url) { "/api/v1/users" }
 
   describe 'Users list' do
-    it 'fetches all the users from the database' do
+    include_examples "authentication required"
+
+    def do_verb
       get url+".json"
+    end
+
+    it 'fetches all the users from the database' do
+      do_verb
       users = JSON.parse(User.all.to_json(:only => [:id, :email]))
       JSON.parse(last_response.body).should eql(users)
     end
 
     it 'replies with status == :ok (200)' do
-      get url+".json"
+      do_verb
       last_response.status.should eql(200)
     end
   end
 
   describe 'Creating a new user' do
+    include_examples "authentication required"
+
     before(:each) do
       @user = FactoryGirl.build(:user)
     end
     
-    def do_post
+    def do_verb
       params = {email: @user.email, password: @user.password, password_confirmation: @user.password_confirmation}
       post url+".json", :user => params
     end
@@ -29,18 +37,18 @@ describe "/api/v1/users.json", :type => :api do
     context 'with valid parametrs' do
       it 'creates an user' do
         expect {
-          do_post
+          do_verb
         }.to change(User,:count).by(1)
       end
 
       it 'replies with status :created (201)' do
         users = User.all.to_json
-        do_post
+        do_verb
         last_response.status.should eq(201)
       end
 
       it "replies with user's details" do
-        do_post
+        do_verb
         body = JSON.parse(last_response.body)
         body['id'].should_not be_nil
         body['email'].should eql(@user.email)
@@ -52,19 +60,19 @@ describe "/api/v1/users.json", :type => :api do
       it "doesn't create a new user" do
         @user.email = ''
         lambda {
-          do_post
+          do_verb
         }.should_not change(User,:count)
       end
 
       it 'replies with status == :unprocessable_entity (422)' do
         @user.email = ''
-        do_post
+        do_verb
         last_response.status.should eq(422)
       end
 
       it 'response body contains the list of errors and an error_msg' do
         @user.email = ''
-        do_post
+        do_verb
         response = JSON.parse(last_response.body)
         response['errors'].should_not be_empty
         response['error_msg'].should eql("Errore durante la creazione dell'utente!")
@@ -74,24 +82,26 @@ describe "/api/v1/users.json", :type => :api do
   end
 
   describe 'Updating an user' do
+    include_examples "authentication required"
+
     before(:each) do
       @user = FactoryGirl.create(:user)
     end
     
-    def do_update
+    def do_verb
       put "#{url}/#{@user.id}.json", :user => {email: @user.email, password: @user.password, password_confirmation: @user.password_confirmation} 
     end
     
     context 'with valid parameters' do 
       it 'replies with status == :ok (200)' do
         @user.email = 'updated@me.com'
-        do_update
+        do_verb
         last_response.status.should eq(200)
       end
 
       it "updates user's details" do
         @user.email = 'updated@me.com'
-        do_update
+        do_verb
         @user.reload
         @user.email.should eq('updated@me.com')        
         puts last_response.body
@@ -106,23 +116,23 @@ describe "/api/v1/users.json", :type => :api do
 
         it "when email is empty" do
           @user.email = ''
-          do_update
+          do_verb
         end
 
         it "when email is already taken" do
           user2 = FactoryGirl.create(:user, email: 'iamme@me.com')
           @user.email = user2.email
-          do_update
+          do_verb
         end
 
         it "when password is empty" do
           @user.password = ''
-          do_update
+          do_verb
         end
 
         it "doesn't update user's details" do
           @user.email = ''
-          do_update
+          do_verb
           @user.reload
           @user.email.should_not eql('')
         end
@@ -131,7 +141,7 @@ describe "/api/v1/users.json", :type => :api do
       context "When user doesn't exists" do
         it "replies with status == :not_found (404)" do
           @user.id = 200
-          do_update
+          do_verb
           last_response.status.should eq(404)
           response = JSON.parse(last_response.body)
         end
@@ -139,7 +149,7 @@ describe "/api/v1/users.json", :type => :api do
 
       it 'response body contains errors' do
         @user.email = ''
-        do_update
+        do_verb
         response = JSON.parse(last_response.body)
         response['errors'].should_not be_empty
       end
@@ -147,17 +157,19 @@ describe "/api/v1/users.json", :type => :api do
   end
 
   describe "Deleting an user" do
+    include_examples "authentication required"
+
     before(:each) { @user = FactoryGirl.create(:user) }
     let(:url) { "/api/v1/users/#{@user.id}.json" }
 
-    def do_delete
+    def do_verb
       delete url
     end
 
     context "When user exists" do
       it "removes user from database" do
         expect {
-          do_delete
+          do_verb
         }.to change(User,:count).by(-1)
         expect {
           User.find(@user.id)
@@ -165,7 +177,7 @@ describe "/api/v1/users.json", :type => :api do
       end
 
       it "replies with status == :no_content (204)" do
-        do_delete
+        do_verb
         last_response.status.should eql(204)
       end
     end
@@ -173,7 +185,7 @@ describe "/api/v1/users.json", :type => :api do
     context "When user doesn't exists" do
         it "replies with status == :not_found (404)" do
           @user.id = 200
-          do_delete
+          do_verb
           last_response.status.should eq(404)
           response = JSON.parse(last_response.body)
         end
--- spec/support/api/helper.rb
+++ spec/support/api/helper.rb
@@ -5,11 +5,27 @@ module ApiHelper
   end
 
   def login_as(user)
-    ApplicationController.any_instance.stub(:current_user).and_return(user)
+    Api::V1::RestrictedController.any_instance.stub(:current_user).and_return(user)
   end
 
   def logout
-    ApplicationController.any_instance.stub(:current_user).and_return(nil)
+    Api::V1::RestrictedController.any_instance.stub(:current_user).and_return(nil)
+  end
+
+  shared_examples "authentication required" do
+    it "when not logged in replies with status == 401 (unauthorized)" do
+      logout
+      do_verb
+      last_response.status.should eq(401)
+    end
+
+    it "when logged in replies with status != 401" do
+      login_as(FactoryGirl.create(:user))
+      do_verb
+      last_response.status.should_not eq(401)
+    end
+
+    before(:each) { login_as(FactoryGirl.create(:user)) }
   end
 
 end
